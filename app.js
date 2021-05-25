7246836d12f2b2bad7e517b45fa3dd33f72a6728
g <umacgamze49@gmail.com>
2021-05-25 12:51:44 +0200

    fertig

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
-const text = document.querySelector(".title");
+/* const text = document.querySelector(".title");
 const changeColor = document.querySelector(".changeColor");
+ */
+const userList = document.querySelector(".name-list ");
 
-const userList = document.querySelectorAll(".name-list li")
+const listInput = document.querySelector(".list-input");
+const addListBtn = document.querySelector(".addListBtn");
 
-console.log(userList);
+addListBtn.addEventListener("click", function () {
+    //Create an li
+const newLi = document.createElement('LI');
+const liContent = document.createTextNode(listInput.value);
+
+    //Add the Input Value
+    newLi.appendChild(liContent);
+    //Attaching the li to the user list
+     userList.appendChild(newLi); 
+
+});
+
+
+/* for(user of userList){
+    user.addEventListener('click', function() {
+       console.log(this); 
+    })
+}
+
+for(user of userList){
+    user.addEventListener('click', function() {
+      this.style.color = 'red'; 
+    })
+} */
+
+
+/* console.log(userList); */
 
 /* text.classList.add("change"); */
-
-text.classList.remove("change");
+/* 
+text.classList.remove("change"); */
 
 /* changeColor.addEventListener('click', function () {
     text.classList.add("change")
 })
  */
-changeColor.addEventListener('click', function () {
+/* changeColor.addEventListener('click', function () {
     text.classList.toggle("change")
-})
+}) */
